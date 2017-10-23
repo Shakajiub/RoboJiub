0f71e6b78d6b1face6189164689ac9908f61f515
Shakajiub <oikarinenjere@gmail.com>
2017-10-24 02:41:23 +0300

    Fix roll minimum

--- src/commands/roll.py
+++ src/commands/roll.py
@@ -5,5 +5,7 @@ def roll(args):
 
     if len(args[2]) > 1:
         max_roll = int(args[2][1])
+        if max_roll < 2:
+            return "@{0} - Minimum roll max is 2! (coin-flip)".format(args[1])
 
     return "@{0} - Rolled {1}!".format(args[1], randrange(1, max_roll))
