aa9739739c68029f98791151850d4794ce633eef
Shakajiub <oikarinenjere@gmail.com>
2018-02-19 13:45:39 +0200

    Fix KeyError on USERSTATE and ROOMSTATE tags

--- src/irc.py
+++ src/irc.py
@@ -29,7 +29,7 @@ class IRC:
             queue.put(("check_for_message() - Can't decode chat message!", 'BG_error'))
             return False
 
-        if len(data) > 1 and len(data[1].split(' ')) > 1:
+        if len(data) > 1 and len(data) < 4 and len(data[1].split(' ')) > 1:
             msg_type = data[1].split(' ')[1]
             msg_data = { 'type': msg_type }
 
